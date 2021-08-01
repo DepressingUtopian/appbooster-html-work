17e5e40bfe40bed979044a2f7f41af2f280c0f7d
DepressingUtopian <DepressingUtopian@gmail.com>
2021-08-01 19:57:20 +0700

    Доработка вертски
    - Переделал расчет размеров блоков, теперь они считаются относительно ширины
    - Переименовал названия элементов
    - Добавил миксины и функцию для расчетов относительных размеров
    - Добавил расчет размера шрифтов
    - Исправил баги вертски

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,15 @@
 import '../style/app.scss';
+
+function init() {
+    calcTableColumnOffset();
+    window.addEventListener('resize', () => calcTableColumnOffset());
+    document.querySelector('.table__column-dates-row.dropdown-content-row').style.borderTop = "1px solid rgba(17,34,62,0.07)";
+}
+
+const calcTableColumnOffset = () => {
+    const tableColumn = document.querySelector(".table__column");
+    const tableColumnDates = document.querySelector(".table__column-dates");
+    tableColumn.style.marginBottom = tableColumnDates.offsetHeight - tableColumnDates.clientHeight + 'px';
+}
+
+init();
--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     output: {
         path: __dirname + '/dist', // Folder to store generated bundle
         filename: 'bundle.js', // Name of generated bundle after build
-        publicPath: '/' // public URL of the output directory when referenced in a browser
+        // publicPath: '/' // public URL of the output directory when referenced in a browser
     },
     module: { // where we defined file patterns and their loaders
         rules: [{
@@ -38,7 +38,8 @@ module.exports = {
     plugins: [ // Array of plugins to apply to build chunk
         new HtmlWebpackPlugin({
             template: __dirname + "/src/public/index.html",
-            inject: 'body'
+            inject: 'body',
+            filename: 'index.html',
         })
     ],
     devServer: { // configuration for webpack-dev-server
